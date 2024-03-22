5be39dd9891fd006c50d5f5cf209048771d06c6d
Dianas <diana.rytko@mail.ru>
2024-03-22 21:05:42 +0300

    requirements

--- kufar/kufar_parser.py
+++ kufar/kufar_parser.py
@@ -125,7 +125,7 @@ class ParserNotebook:
         except Exception as e:
             notebook.price = float(data['adView']['data']['price'].replace('р.', '').replace(' ', ''))
         try:
-            notebook.image = data['adView']['data']['images']['thumbnails']
+            notebook.image = data['adView']['data']['images']
         except Exception as e:
             notebook.image = ''
         try:
